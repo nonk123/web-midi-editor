23a2132deef5d5c17ca52c792735dce92c40a0ee
nonk123 <Sergeyii321@gmail.com>
2022-01-02 20:21:39 +0300

    Implement a track info panel
    Next up, the piano roll.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A MIDI editor on the web.
 
 This app uses the Web MIDI API which is natively supported by Chrome and Opera. Firefox users will need to install the nightly version to use it.
+
+Run the server with `trunk serve`. Then open <http://localhost:8080/> in your browser to use the app.
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod project;
 mod util;
 
 use crate::{
-    project::{Project, TimeSignature},
+    project::{Project, TimeSignature, Track},
     util::{select_get_value, time_signature_options},
 };
 
@@ -18,6 +18,12 @@ enum Msg {
     GrantMidiAccess(MidiAccess),
     RefuseMidiAccess,
     SetOutputDevice(MidiOutput),
+    SelectTrack(usize),
+    DeselectTrack,
+    CreateTrack,
+    DeleteSelectedTrack,
+    RenameSelectedTrack(String),
+    SetSelectedTrackInstrument(u8),
     SetBpm(u32),
     SetTimeSignatureTop(u32),
     SetTimeSignatureBottom(u32),
@@ -27,6 +33,7 @@ struct Model {
     midi_access: Option<MidiAccess>,
     selected_output: Option<MidiOutput>,
     project: Project,
+    selected_track_index: Option<usize>,
     _success_closure: Closure<dyn FnMut(JsValue)>,
     _fail_closure: Closure<dyn FnMut(JsValue)>,
 }
@@ -55,7 +62,7 @@ impl Component for Model {
             link.send_message(Msg::RefuseMidiAccess);
         }) as Box<dyn FnMut(JsValue)>);
 
-        navigator
+        let _ = navigator
             .request_midi_access()
             .expect("request_midi_access")
             .then2(&success, &fail);
@@ -71,12 +78,13 @@ impl Component for Model {
             midi_access: None,
             selected_output: None,
             project,
+            selected_track_index: None,
             _success_closure: success,
             _fail_closure: fail,
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::GrantMidiAccess(midi_access) => {
                 self.midi_access = Some(midi_access);
@@ -105,6 +113,54 @@ impl Component for Model {
 
                 true
             }
+            Msg::SelectTrack(index) => {
+                self.selected_track_index = Some(index);
+
+                true
+            }
+            Msg::DeselectTrack => {
+                self.selected_track_index = None;
+
+                true
+            }
+            Msg::CreateTrack => {
+                self.project.tracks.push(Track {
+                    name: format!("Track {}", self.project.tracks.len() + 1),
+                    notes: Vec::new(),
+                    instrument: 0,
+                });
+
+                let index = self.project.tracks.len() - 1;
+                ctx.link().send_message(Msg::SelectTrack(index));
+
+                true
+            }
+            Msg::DeleteSelectedTrack => {
+                if let Some(index) = self.selected_track_index {
+                    self.project.tracks.remove(index);
+
+                    if self.project.tracks.len() >= 1 {
+                        let index = if index == 0 { 0 } else { index - 1 };
+                        ctx.link().send_message(Msg::SelectTrack(index));
+                    } else {
+                        ctx.link().send_message(Msg::DeselectTrack);
+                    }
+
+                    true
+                } else {
+                    false
+                }
+            }
+            Msg::RenameSelectedTrack(name) => {
+                self.act_on_selected_track_mut(|track| track.name = name.to_string());
+
+                true
+            }
+            Msg::SetSelectedTrackInstrument(instrument) => {
+                self.act_on_selected_track_mut(|track| track.instrument = instrument);
+
+                true
+            }
             Msg::SetBpm(bpm) => {
                 self.project.bpm = bpm;
 
@@ -128,7 +184,12 @@ impl Component for Model {
             html! {
                 <>
                     { self.view_top_bar(ctx) }
-                    <div id="main-view">{ "Hello there!" }</div>
+                    <div id="main-view">
+                        <div class="h-box">
+                            { self.view_piano_roll(ctx) }
+                            { self.view_track_panel(ctx) }
+                        </div>
+                    </div>
                 </>
             }
         } else {
@@ -171,16 +232,14 @@ impl Model {
 
     fn view_no_midi(&self) -> Html {
         html! {
-            <p class="error">
-                { "This app requires MIDI permissions to work" }
-            </p>
+            <p class="error">{ "This app requires MIDI permissions to work" }</p>
         }
     }
 
     fn view_top_bar(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <div id="top-bar">
-                <div class="h-box">
+            <div id="top-bar" class="dark">
+                <div class="h-box dark">
                     { self.view_bpm(ctx) }
                     { self.view_time_signature(ctx) }
                     { self.view_output_selection(ctx) }
@@ -201,7 +260,7 @@ impl Model {
 
         html! {
             <div class="v-box frame">
-                { "BPM" }
+                <span>{ "BPM" }</span>
                 <input type={ "number" } value={ self.project.bpm.to_string() }
                        min={ "1" } max={ "5000" } size={ "5" } { oninput }/>
             </div>
@@ -225,12 +284,12 @@ impl Model {
 
         html! {
             <div class="v-box frame">
-                { "Time Signature" }
+                <span>{ "Time Signature" }</span>
                 <div class="h-box">
                     <select required=true onchange={ on_top_change }>
                         { for top_values }
                     </select>
-                    { "/" }
+                    <span>{ "/" }</span>
                     <select required=true onchange={ on_bottom_change }>
                         { for bottom_values }
                     </select>
@@ -293,7 +352,7 @@ impl Model {
 
         html! {
             <div class="v-box frame">
-                { "MIDI Output" }
+                <span>{ "MIDI Output" }</span>
                 <select required=true { onchange }>
                     { for device_options }
                 </select>
@@ -301,19 +360,167 @@ impl Model {
         }
     }
 
-    fn play_note(&self, note: u8, duration: u32) {
-        let note = JsValue::from_f64(note as f64);
-        let full_velocity = JsValue::from_f64(0x7f as f64);
+    fn view_piano_roll(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="piano-roll">
+                <span>{ "Hello, world!" }</span>
+            </div>
+        }
+    }
 
-        let note_on_message = Array::of3(&JsValue::from_f64(0x90 as f64), &note, &full_velocity);
-        let note_off_message = Array::of3(&JsValue::from_f64(0x80 as f64), &note, &full_velocity);
+    fn view_track_panel(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="project-panel" class="v-box frame">
+                { self.view_project_info() }
+                { self.view_track_select(ctx) }
+                { self.view_track_info(ctx) }
+            </div>
+        }
+    }
+
+    fn view_project_info(&self) -> Html {
+        html! {
+            <div class="v-box-left frame full-width">
+                <div class="full-width">
+                    <span>{ "Project: " }</span>
+                    { self.project.name.to_string() }
+                </div>
+            </div>
+        }
+    }
+
+    fn view_track_select(&self, ctx: &Context<Self>) -> Html {
+        let tracks = self
+            .project
+            .tracks
+            .iter()
+            .enumerate()
+            .map(|(index, track)| {
+                html! {
+                    <option value={ index.to_string() }>{ track.name.to_string() }</option>
+                }
+            });
+
+        let on_select = ctx.link().batch_callback(|event: Event| {
+            select_get_value(event).and_then(|value| {
+                value
+                    .parse()
+                    .ok()
+                    .and_then(|index| Some(Msg::SelectTrack(index)))
+                    .or_else(|| Some(Msg::DeselectTrack))
+            })
+        });
+
+        let create = ctx.link().callback(|_| Msg::CreateTrack);
+        let delete = ctx.link().callback(|_| Msg::DeleteSelectedTrack);
+
+        html! {
+            <div class="v-box-left frame full-width">
+                <span>{ "Tracks:" }</span>
+                {
+                    if self.project.tracks.is_empty() {
+                        html! {
+                            <span>{ "Empty" }</span>
+                        }
+                    } else {
+                        html! {
+                            <select required=true onchange={ on_select }>
+                                { for tracks }
+                            </select>
+                        }
+                    }
+                }
+                <div class="h-box full-width">
+                    <button onclick={ create }>{ "Create" }</button>
+                    <button onclick={ delete }>{ "Delete" }</button>
+                </div>
+            </div>
+        }
+    }
+
+    fn view_track_info(&self, ctx: &Context<Self>) -> Html {
+        let body = self
+            .act_on_selected_track(|track| {
+                let on_track_name_input = ctx.link().batch_callback(|event: InputEvent| {
+                    event
+                        .target_dyn_into::<HtmlInputElement>()
+                        .and_then(|input| Some(Msg::RenameSelectedTrack(input.value())))
+                });
+
+                let on_track_instrument_input = ctx.link().batch_callback(|event: InputEvent| {
+                    event
+                        .target_dyn_into::<HtmlInputElement>()
+                        .and_then(|input| {
+                            input.value().parse().ok().and_then(|mut instrument: i64| {
+                                // Subtract one because we input the instrument number, not index.
+                                instrument -= 1;
+
+                                if instrument >= 0 && instrument <= 127 {
+                                    Some(Msg::SetSelectedTrackInstrument(instrument as _))
+                                } else {
+                                    None
+                                }
+                            })
+                        })
+                });
+
+                html! {
+                    <>
+                        <div class="h-box full-width">
+                            <span>{ "Name: "}</span>
+                            <input value={ track.name.to_string() } oninput={ on_track_name_input }/>
+                        </div>
+                        <div class="h-box full-width">
+                            <span>{ "Instrument: "}</span>
+                            <input type={ "number" } value={ (track.instrument + 1).to_string() }
+                                   min={ "1" } max={ "128" } oninput={ on_track_instrument_input }
+                                   size={ "3" }/>
+                        </div>
+                    </>
+                }
+            })
+            .unwrap_or_else(|| {
+                html! {
+                    <span>{ "No track selected" }</span>
+                }
+            });
+
+        html! {
+            <div class="v-box-left frame full-width">
+                { body }
+            </div>
+        }
+    }
+
+    fn act_on_selected_track<R>(&self, action: impl Fn(&Track) -> R) -> Option<R> {
+        if let Some(index) = self.selected_track_index {
+            Some(action(&self.project.tracks[index]))
+        } else {
+            None
+        }
+    }
+
+    fn act_on_selected_track_mut<R>(&mut self, action: impl Fn(&mut Track) -> R) -> Option<R> {
+        if let Some(index) = self.selected_track_index {
+            Some(action(&mut self.project.tracks[index]))
+        } else {
+            None
+        }
+    }
+
+    fn play_note(&self, note: u8, duration: u32) {
+        let note = JsValue::from_f64(note as _);
+        let full_velocity = JsValue::from_f64(0x7f as _);
+
+        let note_on_message = Array::of3(&JsValue::from_f64(0x90 as _), &note, &full_velocity);
+        let note_off_message = Array::of3(&JsValue::from_f64(0x80 as _), &note, &full_velocity);
 
         let output = self.selected_output.as_ref().unwrap();
 
         output.send(&note_on_message).ok();
 
         output
-            .send_with_timestamp(&note_off_message, duration as f64)
+            .send_with_timestamp(&note_off_message, duration as _)
             .ok();
     }
 }
